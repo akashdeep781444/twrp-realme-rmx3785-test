1f61b9e13fcb02f2eb0cc13e0a3b5169b63f1be9
akashdeep781444 <akashdeepsingh781445@gmail.com>
2026-02-02 20:22:41 +0530

    Update AndroidProducts.mk

--- device/realme/RE5C6CL1/AndroidProducts.mk
+++ device/realme/RE5C6CL1/AndroidProducts.mk
@@ -1,7 +1,5 @@
-# device/realme/RE5C6CL1/AndroidProducts.mk
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_RE5C6CL1.mk
+    $(LOCAL_DIR)/omni_RE5C6CL1.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RE5C6CL1-eng
+    omni_RE5C6CL1-eng
