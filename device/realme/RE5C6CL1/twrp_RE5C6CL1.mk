1c9288d4256e2f046c0d2bba894c4eca6c213d4f
akashdeep781444 <akashdeepsingh781445@gmail.com>
2026-02-02 19:35:58 +0530

    Update twrp_RE5C6CL1.mk

--- device/realme/RE5C6CL1/twrp_RE5C6CL1.mk
+++ device/realme/RE5C6CL1/twrp_RE5C6CL1.mk
@@ -1,4 +1,7 @@
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+# device/realme/RE5C6CL1/twrp_RE5C6CL1.mk
+
+# Inherit from device.mk in the SAME directory
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_DEVICE := RE5C6CL1
 PRODUCT_NAME := twrp_RE5C6CL1
