d8fdf15f676ca7a389af37334c6a66f6d5c759ff
akashdeep781444 <akashdeepsingh781445@gmail.com>
2026-02-02 20:21:56 +0530

    Update device.mk

--- device/realme/RE5C6CL1/device.mk
+++ device/realme/RE5C6CL1/device.mk
@@ -1,26 +1,5 @@
-# device/realme/RE5C6CL1/device.mk
-
-# Inherit from common TWRP
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Device identifier
 PRODUCT_DEVICE := RE5C6CL1
-PRODUCT_NAME := twrp_RE5C6CL1
+PRODUCT_NAME := omni_RE5C6CL1
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3785
 PRODUCT_MANUFACTURER := realme
-
-# Copy kernel
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/kernel:kernel
-
-# Recovery packages
-PRODUCT_PACKAGES += \
-    twrp \
-    busybox \
-    toybox
-
-# For A/B devices
-PRODUCT_PACKAGES += \
-    bootctrl.$(TARGET_BOARD_PLATFORM) \
-    bootctrl.$(TARGET_BOARD_PLATFORM).recovery
